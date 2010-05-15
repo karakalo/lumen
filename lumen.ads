c13fdcc789de15f71c8acb70e8684bf007ddc8f8
Chip Richards <chipr@niestu.com>
2010-05-15 01:11:18 -0700

    Initial version, still empty

---
+++ lumen.ads
@@ -0,0 +1,13 @@
+
+-- Lumen -- A simple graphical user interface library based on OpenGL
+--
+-- Chip Richards, NiEstu, Phoenix AZ, Spring 2010
+
+-- Lumen would not be possible without the support and contributions of a cast
+-- of thousands, including Rod Kay.
+
+package Lumen is
+   pragma Pure (Lumen);
+
+end Lumen;
+
