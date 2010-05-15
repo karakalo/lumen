1e78361f9324371b2fe0d054ce044e95bad475f1
Chip Richards <chipr@niestu.com>
2010-05-15 06:42:50 -0700

    Reworded credit comment and added license text

--- lumen.ads
+++ lumen.ads
@@ -4,7 +4,23 @@
 -- Chip Richards, NiEstu, Phoenix AZ, Spring 2010
 
 -- Lumen would not be possible without the support and contributions of a cast
--- of thousands, including Rod Kay.
+-- of thousands, including and primarily Rod Kay.
+
+-- This code is covered by the ISC License:
+--
+-- Copyright (c) 2010, NiEstu
+--
+-- Permission to use, copy, modify, and/or distribute this software for any
+-- purpose with or without fee is hereby granted, provided that the above
+-- copyright notice and this permission notice appear in all copies.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+-- WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+-- MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+-- SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+-- WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+-- ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+-- IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 package Lumen is
    pragma Pure (Lumen);
