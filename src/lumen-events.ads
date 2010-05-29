900ca7c9fbec6842401033adb057e1b4f3a63d03
Chip Richards <chipr@niestu.com>
2010-05-29 14:48:15 -0700

    Changed license text to real copyright symbol

--- src/lumen-events.ads
+++ src/lumen-events.ads
@@ -5,7 +5,7 @@
 
 -- This code is covered by the ISC License:
 --
--- Copyright (c) 2010, NiEstu
+-- Copyright © 2010, NiEstu
 --
 -- Permission to use, copy, modify, and/or distribute this software for any
 -- purpose with or without fee is hereby granted, provided that the above
