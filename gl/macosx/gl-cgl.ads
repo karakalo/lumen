b96098cef1c3db6089cef2ad9c718cfef7ee8565
Felix Krause <flyx@isobeef.org>
2011-08-29 22:37:29 +0200

    added stuff from CGLCurrent.h and license

--- gl/macosx/gl-cgl.ads
+++ gl/macosx/gl-cgl.ads
@@ -1,3 +1,17 @@
+--  Copyright (c) 2011, Felix Krause <flyx@isobeef.org>
+--
+--  Permission to use, copy, modify, and/or distribute this software for any
+--  purpose with or without fee is hereby granted, provided that the above
+--  copyright notice and this permission notice appear in all copies.
+--
+--  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+--  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+--  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+--  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+--  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+--  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+--  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
 with System;
 with Interfaces.C.Pointers;
 with Interfaces.C.Extensions;
@@ -350,6 +364,9 @@ package GL.CGL is
    function CGLErrorString (error : CGLError)
                             return Interfaces.C.Strings.chars_ptr;
 
+   function CGLSetCurrentContext (ctx : CGLContextObject) return CGLError;
+
+   function CGLGetCurrentContext return CGLContextObject;
 
 private
    C_Enum_Size : constant := 32;
@@ -560,4 +577,7 @@ private
 
    pragma Import (C, CGLGetVersion, "CGLGetVersion");
    pragma Import (C, CGLErrorString, "CGLErrorString");
+
+   pragma Import (C, CGLSetCurrentContext, "CGLSetCurrentContext");
+   pragma Import (C, CGLGetCurrentContext, "CGLGetCurrentContext");
 end GL.CGL;
